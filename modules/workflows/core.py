3025d2d93e0cd2243c305455d4e47969610c1f65
Jayasooryan TM <jayasooryan.tm@outlook.com>
2022-09-07 04:07:29 +0100

    beta version

--- modules/workflows/core.py
+++ modules/workflows/core.py
@@ -5,6 +5,8 @@ monitor_db_path = "files/database/db_monitors.xlsx"
 allocation_template_path = "files/database/db_allocation_template.xlsx"
 subject_db_path = "files/database/db_subjects.xlsx"
 powerbi_data_path = "files/input/data.xlsx"
+output_data_path = "files/output/Allocation.xlsx"
+shortname_data_path = "files/database/db_shortnames.xlsx"
 
 
 def allocation_process(logger) -> None:
@@ -20,31 +22,70 @@ def allocation_process(logger) -> None:
         logger.info("Loading monitoring files...")
         monitor_data = pd.read_excel(monitor_db_path)
         data = pd.read_excel(powerbi_data_path)
+        shortnames = pd.read_excel(shortname_data_path)
 
+        logger.info("Removing closed monitoring data...")
+        data = data[data["Status"] != "Closed"].copy()
         logger.info("Sorting data...")
         data.sort_values(by=["Subject Group", "Component"], inplace=True)
 
-        grouped_data = data.groupby(by=['Subject Group', 'Component'], axis=0)
         date = dt.today().strftime("%d-%b")
-        #-----------------------------------------------------------
-        monitor_data[date] = monitor_data[date].astype('string')
+        monitor_data[date] = monitor_data[date].astype("string")
 
-        
-        for key, value in grouped_data:
-            # key[0], key[1] = 'Subject Group', 'Component'
-            
-            subject = key[0]
-            
-            print('-----------------------monitor data------------------------')
-            temp = monitor_data[monitor_data[date] == 'Yes']
-            available_monitors = temp[temp["subject_group"]==subject]
+        for index, row in data.iterrows():
+            available_monitors = monitor_data[monitor_data[date] == "Yes"].copy(
+                deep=False
+            )
 
-            if available_monitors:
-                print(available_monitors)
+            names_array = shortnames[
+                shortnames["short_name"].apply(
+                    lambda x: str(x) == str(row["locked_by"])
+                )
+            ]
 
-            
-                                
+            if not names_array.empty:
+                logger.info("Assigning locked monitor data...")
+                name = names_array.iloc[0, 1]
+                output = monitor_data[monitor_data[date] == "Yes"].copy(deep=False)
+                output = output[output["monitor_name"] == name].copy(deep=False)
+
+                if not output.empty:
+                    data.loc[index, "Monitor"] = name
+                    data.loc[index, "Comment"] = "Already locked batch"
+                else:
+                    data.loc[
+                        index, "Comment"
+                    ] = "Monitor previously made corrections - not in today, so reallocated"
+
+        for index, row in data.iterrows():
+            available_monitors = monitor_data[
+                (monitor_data[date] == "Yes")
+                & (monitor_data["subject_group"] == row["Subject Group"])
+            ].copy(deep=False)
+
+            if not available_monitors.empty:
+                for i, monitor_row in available_monitors.iterrows():
+
+                    name = str(monitor_row["monitor_name"])
+                    standardised = str(monitor_row["standardised"]).split(",")
+                    current_load = len(
+                        data[data["Monitor"].apply(lambda x: str(x) == name)]
+                    )
+
+                    if (
+                        (current_load < 10)
+                        and (row["Component"] not in standardised)
+                        and (str(row["Monitor"]).strip() == "nan")
+                    ):
+                        data.loc[index, "Monitor"] = name
+                        logger.info("1 Monitor assigned...")
+
+            # saving allocation data
+        data.to_excel(output_data_path, index=False)
+        logger.info("Allocation data saved successfully.")
+
         return None
+
     except Exception as e:
         status_allocation_process = "Failed"
         logger.error(f"Error occured: {e}")
